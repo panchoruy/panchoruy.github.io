672578e036ca3af58dd4a6bc5c0fcb53205d22c1
Pancho <panchifoco@gmail.com>
2019-11-26 16:53:14 -0800

    new stuff

--- panchoruy-website/src/app/photo.service.ts
+++ panchoruy-website/src/app/photo.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const API_KEY = "35aaa78f22e30b2123c479fccac9a369";
+const JSON_ARGS = "format=json&nojsoncallback=1";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class PhotoService {
-  api_key: string;
 
   constructor(private http: HttpClient) {
-    this.api_key = "35aaa78f22e30b2123c479fccac9a369";
-
   }
 
-  getPhoto(id: string) { 
-    var base_url = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+  getPhotoSizesObservable(
+    photo_id: string, 
+    api_key: string = API_KEY) {
 
+    var request_url = `https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${api_key}&photo_id=${photo_id}&${JSON_ARGS}`
+    return this.http.get(request_url)
   }
 
-  searchPhoto() {
-    var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=35aaa78f22e30b2123c479fccac9a369&user_id=panchoruy&tags=website&format=json&nojsoncallback=1";
-    return this.http.get(url);
+  getPhotoListByTagsObservable(
+    tags: string = "website",
+    user_id: string = "panchoruy", 
+    api_key: string = API_KEY) { 
+
+    var base_url = "https://www.flickr.com/services/rest/?method=flickr.photos.search" 
+    var request_url = `${base_url}&api_key=${api_key}&user_id=${user_id}&tags=${tags}&${JSON_ARGS}`
+    return this.http.get(request_url);
   }
 }
--- panchoruy-website/src/app/photography/photography.component.ts
+++ panchoruy-website/src/app/photography/photography.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './../photo.service';
+import { forkJoin } from 'rxjs';
+
 
 @Component({
   selector: 'app-photography',
@@ -8,15 +10,41 @@ import { PhotoService } from './../photo.service';
 })
 export class PhotographyComponent implements OnInit {
 
-  photoList: any;
-
   constructor(private photoService: PhotoService) { }
 
+  innerWidth: any;
+
   ngOnInit() {
-    this.photoService.searchPhoto().subscribe((data: any) => {
-      console.log(data.constructor);
-      this.photoList = data.photos.photo;
-      console.log(this.photoList);
+    this.photoService.getPhotoListByTagsObservable().subscribe((data: any) => {
+      this.calculateAndDisplayRows(data.photos.photo);
+    });
+    this.innerWidth = window.innerWidth;
+  }
+
+  private calculateAndDisplayRows(photoList: any) {
+    var photoObservables = photoList.map(photoInfo => 
+      this.photoService.getPhotoSizesObservable(photoInfo.id))
+    
+    forkJoin(photoObservables).subscribe((data: any) => {
+      var photoDimensions = data.map(element => element.sizes.size);
+      var rows = [];
+      var row = {width: 0, photoIndexes: []};
+      for (var index = 0; index < photoDimensions.length; index++) {
+        var original = photoDimensions[index].find(size => size.label == "Original");
+        var ratio = original.width / original.height;
+        var scaledBaseWidth = ratio * 400;
+        row.width += scaledBaseWidth;
+        row.photoIndexes.push({index}); 
+        if (row.width >= this.innerWidth) {
+          rows.push(this.normalizeRowDimensions(row));
+          row = {width: 0, photoIndexes: []};
+        }
+      }
     });
   }
+
+  private normalizeRowDimensions(row){
+    return row;
+  }
+
 }
