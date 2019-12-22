15d1af61d37f5e4fd183f1bd034682806a9ea781
panchoruy <panchifoco@gmail.com>
2019-12-22 14:27:24 -0800

    Removed home button to replace with toolbar

--- panchoruy-website/src/app/app.module.ts
+++ panchoruy-website/src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Angular Material Components
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 // Declarations
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { PhotographyComponent } from './photography/photography.component';
 import { HomeComponent } from './home/home.component';
-import { HomeButtonComponent } from './home-button/home-button.component';
+import { MenuToolbarComponent } from './menu-toolbar/menu-toolbar.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AboutComponent } from './about/about.component';
+import { PhotoComponent } from './photo/photo.component';
+
 // Providers
 import { PhotoService } from './photo.service';
-import { PhotoComponent } from './photo/photo.component';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +29,7 @@ import { PhotoComponent } from './photo/photo.component';
     MainMenuComponent,
     PhotographyComponent,
     HomeComponent,
-    HomeButtonComponent,
+    MenuToolbarComponent,
     ProjectsComponent,
     PageNotFoundComponent,
     AboutComponent,
@@ -33,7 +39,9 @@ import { PhotoComponent } from './photo/photo.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [
     PhotoService
