7f22193eabfdb2b3eff85acfe7a78d4ee82c651c
Pancho <panchifoco@gmail.com>
2020-11-29 13:04:30 -0500

    Added animated logo on the home page

--- panchoruy-website/src/app/home/home.component.ts
+++ panchoruy-website/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let lottie: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +12,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Enable logo animation.
+    lottie.loadAnimation({
+      container: document.getElementById("logo"), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animated_logo.json' // the path to the animation json
+    });
   }
 
 }
