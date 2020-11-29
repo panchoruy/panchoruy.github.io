095cbf3b76d6f53e7a3c01444f9d4c96bc9052ef
Pancho <panchifoco@gmail.com>
2020-11-29 12:01:41 -0500

    Ran ng update

--- panchoruy-website/src/app/menu-toolbar/menu-toolbar.component.spec.ts
+++ panchoruy-website/src/app/menu-toolbar/menu-toolbar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MenuToolbarComponent } from './menu-toolbar.component';
 
@@ -6,7 +6,7 @@ describe('MenuToolbarComponent', () => {
   let component: MenuToolbarComponent;
   let fixture: ComponentFixture<MenuToolbarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MenuToolbarComponent ]
     })
--- panchoruy-website/src/app/photography/photography.component.spec.ts
+++ panchoruy-website/src/app/photography/photography.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PhotographyComponent } from './photography.component';
 
@@ -6,7 +6,7 @@ describe('PhotographyComponent', () => {
   let component: PhotographyComponent;
   let fixture: ComponentFixture<PhotographyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PhotographyComponent ]
     })
